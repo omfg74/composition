81dee06384f69842d39b6b6de3444f51b33f2402
omfg <podsh74@gmail.com>
2022-03-14 20:15:32 +0500

    add game repository

---
+++ app/src/main/java/com/omfgdevelop/composition/presentation/GameFragment.kt
@@ -0,0 +1,24 @@
+package com.omfgdevelop.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.omfgdevelop.composition.R
+
+class GameFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
