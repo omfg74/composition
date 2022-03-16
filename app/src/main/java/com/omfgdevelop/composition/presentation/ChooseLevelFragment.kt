13ed254228bcd20afcff1270d188a60b573b02ef
omfg <podsh74@gmail.com>
2022-03-16 13:02:16 +0500

    view binding implementation

--- app/src/main/java/com/omfgdevelop/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/omfgdevelop/composition/presentation/ChooseLevelFragment.kt
@@ -5,15 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.omfgdevelop.composition.R
+import com.omfgdevelop.composition.databinding.FragmentChooseLevelBinding
 
 class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("binding is null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
